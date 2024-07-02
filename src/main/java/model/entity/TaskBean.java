e0ced5fc26fdede4a3bc5839f4fbb1f9617fbf12
comsize-furukawa <masami.furukawa@comsize.com>
2024-07-02 11:33:18 +0900

    タスク一覧関連DAO およびBeanの追加

--- src/main/java/model/entity/TaskBean.java
+++ src/main/java/model/entity/TaskBean.java
@@ -1,5 +1,62 @@
 package model.entity;
 
+import java.sql.Date;
 public class TaskBean {
 
+	private String taskName;
+	private String categoryName;
+	private Date limitDate;
+	private String userName;
+	private String statusName;
+	private String memo;
+	
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public Date getLimitDate() {
+		return limitDate;
+	}
+
+	public void setLimitDate(Date limitDate) {
+		this.limitDate = limitDate;
+	}
+	
 }
