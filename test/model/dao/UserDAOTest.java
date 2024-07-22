95099029d2df0e0d95a88e0adfe702b0600c1fb7
comsize-kazuma-kobayashi <kazuma.kobayashi@comsize.com>
2024-07-22 15:26:59 +0900

    UserDAOのテストクラスの追加

--- test/model/dao/UserDAOTest.java
+++ test/model/dao/UserDAOTest.java
@@ -2,13 +2,37 @@ package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
+
+import model.entity.UserBean;
 
 class UserDAOTest {
 
+	UserDAO userDao = new UserDAO();
+	UserBean userBean = null;
+
 	@Test
-	void test() {
-		fail("まだ実装されていません");
+	void test1_login() {
+		try {
+			userBean = userDao.login("kobayashi", "kazuma");
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+
+		}
+		assertEquals("小林一馬", userBean.getUserName());
+
 	}
 
+	@Test
+	void test2_login() {
+
+		try {
+			userBean = userDao.login("", "");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		assertNull(userBean);
+	}
 }
